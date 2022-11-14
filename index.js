6d1a8055d2b87c843d283817eb54799ec2124ba6
bernard <bernard.munyiri@gmail.com>
2022-11-14 11:48:13 +0300

    commit 3 -A Delete req to remove a people

--- index.js
+++ index.js
@@ -5,17 +5,20 @@ app.use(Express.json());
 
 let watu =[
 
-{
+    {
+    id:1,
 name:"Kelele Mingi",
 age:35,
 status:true
 },
-{
+    {
+     id:2,
 name:"Kamutu Kadogo",
 age:15,
 status:false
 },
-{
+    {
+     id:3,
 name:"Mugongo Muture",
 age:65,
 status:true
@@ -27,10 +30,13 @@ status:true
 //     res.send("Kimeumana")
 // })
 
+// Commit - 1 to display watu list 
 app.get( "/people", ( req, res ) => {
     res.json(watu)
 })
 
+
+//Commit - 2 To add a person to watu list
 app.post( "/people", ( req, res ) => {
   
     const NewWatu = req.body
@@ -41,6 +47,19 @@ app.post( "/people", ( req, res ) => {
     
 })
 
+//Commit -3 To remove a Person from watu list
+app.delete( "/people/:id", ( req, res ) => {
+    
+    const id = req.params.id
+
+    for ( let i = 0; i < watu.length; i++ ){
+        if ( watu[ i ].id == id ) { 
+              watu.splice(i,1)  
+        }
+    
+    }
+      res.json(watu)
+})
 
 app.listen( '3001', () => {
     console.log("Server Running at port 3001")
